4a14350b18a37e5dd0016746c8bc8ee07862b708
Sergio Calderon <bs.sergiocalderon@gmail.com>
2025-07-21 20:43:13 -0300

    Dockerize app

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import connectToDatabase from "./config/database.js";
 dotenv.config();
 
 const app = e();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(e.json());
