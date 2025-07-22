6810c2b2d87e9c77b106b6ab59e5c07845ccddb0
Sergio Calderon <bs.sergiocalderon@gmail.com>
2025-07-21 21:09:30 -0300

    Add PUT route for locations

--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -29,4 +29,21 @@ router.post('/', (req, res) => {
   });
 });
 
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, latitude, longitude, zipcode, notes } = req.body;
+
+  Location.findByIdAndUpdate(id, { name, latitude, longitude, zipcode, notes }, { new: true })
+    .then(updatedLocation => {
+      if (!updatedLocation) {
+        return res.status(404).json({ message: 'Location not found' });
+      }
+      res.json(updatedLocation);
+    })
+    .catch(err => {
+      res.status(400).json({ message: 'Error updating location', error: err.message });
+    }
+  );
+});
+
 export default router;
