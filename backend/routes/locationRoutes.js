0417dbf5e34a7bd79b4138f7c80de79f063b6bdc
Sergio Calderon <bs.sergiocalderon@gmail.com>
2025-07-26 18:39:40 -0300

    Create location controller

--- backend/routes/locationRoutes.js
+++ backend/routes/locationRoutes.js
@@ -1,49 +1,10 @@
 import e from "express";
-import Location from "../models/Location.js";
+import { createLocation, getAllLocations, updateLocation } from "../controller/locationController.js";
 
 const router = e.Router();
 
-router.get('/', (req, res) => {
-  Location.find().sort({ zipcode: 1, name: 1 }).then(locations => {
-    res.json(locations);
-  }).catch(err => {
-    res.status(500).json({ message: 'Error fetching locations', error: err.message });
-  });
-})
-
-router.post('/', (req, res) => {
-  const { name, latitude, longitude, zipcode, notes } = req.body;
-
-  const location = new Location({
-    name,
-    latitude,
-    longitude,
-    zipcode,
-    notes
-  });
-
-  location.save().then(savedLocation => {
-    res.status(201).json(savedLocation);
-  }).catch(err => {
-    res.status(400).json({ message: 'Error creating location', error: err.message });
-  });
-});
-
-router.put('/:id', (req, res) => {
-  const { id } = req.params;
-  const { name, latitude, longitude, zipcode, notes } = req.body;
-
-  Location.findByIdAndUpdate(id, { name, latitude, longitude, zipcode, notes }, { new: true })
-    .then(updatedLocation => {
-      if (!updatedLocation) {
-        return res.status(404).json({ message: 'Location not found' });
-      }
-      res.json(updatedLocation);
-    })
-    .catch(err => {
-      res.status(400).json({ message: 'Error updating location', error: err.message });
-    }
-  );
-});
+router.get('/', getAllLocations);
+router.post('/', createLocation);
+router.put('/:id', updateLocation);
 
 export default router;
