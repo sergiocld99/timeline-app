0921e29ba840ab4da941914284eaa0827d0a243a
Sergio Calderon <bs.sergiocalderon@gmail.com>
2025-11-03 04:24:05 -0300

    add chart by date

--- frontend-v2/src/components/VisitStats.tsx
+++ frontend-v2/src/components/VisitStats.tsx
@@ -44,7 +44,7 @@ const getEachHourOfVisit = ({ arrivalTime, departureTime }: Visit): HourPart[] =
 
   hours.push({ hour: normalizeHour(startTime.hour), completitude: (60 - startTime.minutes) / 60 });
   hours.push({ hour: normalizeHour(endTime.hour), completitude: endTime.minutes / 60 });
-  
+
   for (let i = startTime.hour + 1; i < endTime.hour; i++) {
     hours.push({ hour: normalizeHour(i), completitude: 1 });
   }
@@ -74,16 +74,34 @@ const VisitStats = ({ visitsData }: Props) => {
   const { visits } = visitsData;
 
   const chartHours = [];
+  const chartDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  for (let i = 0; i < 24; i++) {
-    chartHours.push(normalizeHour(i));
-  }
+  for (let i = 0; i < 24; i++) { chartHours.push(normalizeHour(i)) }
 
   const topThreeLocations = calculateBestLocations(visits)
   if (!topThreeLocations[0]) topThreeLocations[0] = ''
   if (!topThreeLocations[1]) topThreeLocations[1] = ''
   if (!topThreeLocations[2]) topThreeLocations[2] = ''
   if (!topThreeLocations[3]) topThreeLocations[3] = ''
+
+  const weightByDay = visits.reduce((acc, v) => {
+    const dayOfWeek = new Date(v.arrivalTime).getDay()
+    const normalizedDay = chartDays[dayOfWeek]
+    const chartLocation = topThreeLocations.includes(v.location.name) ? v.location.name : 'others';
+
+    if (!acc[normalizedDay]) {
+      acc[normalizedDay] = {
+        [topThreeLocations[0]]: 0,
+        [topThreeLocations[1]]: 0,
+        [topThreeLocations[2]]: 0,
+        [topThreeLocations[3]]: 0,
+        others: 0
+      }
+    }
+
+    acc[normalizedDay][chartLocation] += v.durationMinutes
+    return acc;
+  }, {} as ChartSource)
 
   const weightByHour = visits.reduce((acc, visit) => {
     const hours = getEachHourOfVisit(visit);
@@ -122,6 +140,22 @@ const VisitStats = ({ visitsData }: Props) => {
     others: roundChartValue(weightByHour[hour].others || 0)
   }));
 
+  const chartDataByDate = chartDays.map(day => !weightByDay[day] ? ({
+    day,
+    red: 0,
+    orange: 0,
+    yellow: 0,
+    blue: 0,
+    others: 0
+  }) : ({
+    day,
+    red: roundChartValue(weightByDay[day][topThreeLocations[0]] || 0),
+    orange: roundChartValue(weightByDay[day][topThreeLocations[1]] || 0),
+    yellow: roundChartValue(weightByDay[day][topThreeLocations[2]] || 0),
+    blue: roundChartValue(weightByDay[day][topThreeLocations[3]] || 0),
+    others: roundChartValue(weightByDay[day].others || 0)
+  }));
+
   const chartConfig = {
     red: {
       label: topThreeLocations[0],
@@ -148,20 +182,52 @@ const VisitStats = ({ visitsData }: Props) => {
   return (
     <Card className="w-full bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-700">
       <CardContent className="h-[300px] flex items-center justify-center">
-        <ChartContainer config={chartConfig} className="min-h-[300px] max-h-[300px] max-w-full min-w-full">
+        <ChartContainer config={chartConfig} className="min-h-[300px] max-h-[300px] max-w-3/5 min-w-3/5">
           <BarChart accessibilityLayer data={chartData}>
             <CartesianGrid vertical={false} />
             <XAxis
               dataKey="hour"
-              tickLine={false}
               tickMargin={10}
-              axisLine={false}
               tickFormatter={(value) => value.slice(0, 3)}
             />
-            <ChartTooltip
-              content={<ChartTooltipContent indicator="dashed" />}
-            />
+            <ChartTooltip content={<ChartTooltipContent />} />
             <ChartLegend content={<ChartLegendContent />} />
+            <Bar
+              dataKey="red"
+              stackId="a"
+              fill="var(--color-red)"
+            />
+            <Bar
+              dataKey="orange"
+              stackId="a"
+              fill="var(--color-orange)"
+            />
+            <Bar
+              dataKey="yellow"
+              stackId="a"
+              fill="var(--color-yellow)"
+            />
+            <Bar
+              dataKey="blue"
+              stackId="a"
+              fill="var(--color-blue)"
+            />
+            <Bar
+              dataKey="others"
+              stackId="a"
+              fill="var(--color-others)"
+            />
+          </BarChart>
+        </ChartContainer>
+        <ChartContainer config={chartConfig} className="min-h-[270px] max-h-[270px] mx-auto max-w-1/4 min-w-1/4">
+          <BarChart accessibilityLayer data={chartDataByDate}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="day"
+              tickMargin={10}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip content={<ChartTooltipContent />} />
             <Bar
               dataKey="red"
               stackId="a"
