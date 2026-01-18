02686a20d6958f955bc92c3f84b556268b2c229d
Sergio Calderon <bs.sergiocalderon@gmail.com>
2026-01-17 23:13:14 -0300

    front: use new stats endpoint

--- frontend-v2/src/constants/index.ts
+++ frontend-v2/src/constants/index.ts
@@ -1,3 +1,4 @@
 export const backendBaseUrl = 'http://localhost:3000/api';
+export const v2BaseUrl = 'http://localhost:8081/api/v2';
 
 export const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
