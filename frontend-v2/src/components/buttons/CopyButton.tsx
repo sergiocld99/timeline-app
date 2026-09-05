f21a71a59cad3f9ae68927447eac51763892c47a
Sergio Calderon <bs.sergiocalderon@gmail.com>
2026-09-05 14:00:48 -0300

    feat: show green check icon on copy coordinates button

--- frontend-v2/src/components/buttons/CopyButton.tsx
+++ frontend-v2/src/components/buttons/CopyButton.tsx
@@ -1,21 +1,25 @@
 import type { ButtonProps } from "@/types/props"
 
-import { Copy } from "lucide-react"
+import { Check, Copy } from "lucide-react"
 import { useTranslations } from "next-intl"
 
 import { Button } from "@/components/ui/button"
 
-const CopyButton = ({handleClick}: ButtonProps) => {
+type CopyButtonProps = ButtonProps & {
+  copied: boolean
+}
+
+const CopyButton = ({ handleClick, copied }: CopyButtonProps) => {
   const t = useTranslations("Actions");
   return (
     <Button
       onClick={handleClick}
       size="sm"
       variant="outline"
-      title={t("copyCoordinates")}
+      title={copied ? t("copiedCoordinates") : t("copyCoordinates")}
       className="h-6 w-6 p-0 mx-3"
     >
-      <Copy className="h-3 w-3" />
+      {copied ? <Check className="h-3 w-3 text-green-600" /> : <Copy className="h-3 w-3" />}
     </Button>
   )
 }
